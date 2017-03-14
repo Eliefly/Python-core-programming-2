65ff621cbfd1561fbc75a47dfc5d81d443ad33de
eliefly <eliefly@163.com>
2017-03-15 00:54:35 +0800

    update

---
+++ chapter-2/2.py
@@ -0,0 +1,29 @@
+# -*- conding: utf-8 -*-
+# !/usr/bin/python3
+
+# 2-5
+# n = 0
+# while n <= 10:
+#     print(n, end=' ')
+#     n += 1
+
+# for n in range(11):
+#     print(n)
+
+# 2-6
+# number = input('Please input a number:')
+# n = int(number)
+
+# if n == 0:
+#     print('this number is 0')
+# elif n > 0:
+#     print('this number is postive')
+# elif n < 0:
+#     print('this number is negative')
+
+# 2-7
+str = '1234'
+n = 0
+while n < len(str):
+    print(str[n])
+    n += 1

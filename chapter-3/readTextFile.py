8faf409ce13596839875ce8a3b2490d1cd24e930
eliefly <Eliefly@users.noreply.github.com>
2017-03-15 16:33:06 +0800

    Create readTextFile.py

---
+++ chapter-3/readTextFile.py
@@ -0,0 +1,17 @@
+# -*- conding: utf-8 -*-
+# python3.5
+
+'readTextFile.py -- read and display text file'
+
+fname = input('Enter filename:')
+print('Attempt to open file for reading...')
+
+try:
+    fobj = open(fname, 'r')
+except IOError as e:
+    print('file open error:', e)
+else:
+    # display contents to the screen
+    for eachLine in fobj:
+        print(eachLine)
+    fobj.close()
